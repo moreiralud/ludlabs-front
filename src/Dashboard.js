98f9af052860d1f789226dece45bd11c8b5865b7
moreiralud <ludmila.brmr@gmail.com>
2023-10-26 11:20:59 -0300

    refactoring classes

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,25 +1,34 @@
 import React, { useEffect } from 'react';
-import Cookies from 'js-cookie';
+import { Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
-
+import Cookies from 'js-cookie';
+import './Dashboard.css';
 
 function Dashboard() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-      const isAuthenticated = Cookies.get('authenticated')  === 'true';
-      if (!isAuthenticated) {
+  useEffect(()=>{
+    if(Cookies.get('authenticated') !== 'true'){
+      navigate('/');
+    }
+  });
 
-        // Se o cookie de autenticação não existir, redirecione o usuário para a página de login
-        navigate('/');
-      }
-    }, []);
-  
-    return (
-      <div>
-        {/* Conteúdo da Dashboard */}
+  const handleLogout = () => {
+    Cookies.remove('authenticated');
+    navigate('/');
+  };
+
+  return (
+    <div className="dashboard">
+      <div className="dashboard_box">
+        <h1>Seja Bem-Vindo!</h1>
+        <p>O login foi bem-sucedido! Agora você está na página de boas-vindas.</p>
+        <Button variant="primary" onClick={handleLogout}>
+          Sair
+        </Button>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default Dashboard;
--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form, Alert } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
 
@@ -8,57 +8,60 @@ function LoginForm(props) {
   const navigate = useNavigate();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     try {
       const response = await axios.post('http://localhost:8080/api/v1/auth/login', {
         username,
         password,
       });
-
-      if (response.status === 200) {
-        Cookies.set('authenticated', true);
-        navigate('/dashboard');
-      } else {
-        alert('Erro na autenticação');
-      }
+      Cookies.set('authenticated', true);
+      navigate('/dashboard');
     } catch (error) {
-      console.error('Erro na requisição à API:', error);
-      alert('Erro inesperado');
+      if (error.response && error.response.status === 401) {
+        setError('Credenciais inválidas. Verifique seu nome de usuário e senha.');
+      } else{
+        setError('Ocorreu um erro. Tente mais tarde.');
+      }
     }
   };
 
   return (
-    <>
-    <h1>Login Form</h1>
-    <Form onSubmit={handleSubmit}>
-      <Form.Group controlId="formBasicUsername">
-        <Form.Label>Usuário</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Digite seu usuário"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </Form.Group>
+    <div className="form">
+      <div className="login_box">
+        <h1>Login</h1>
+        <p>Por favor, entre com suas credenciais para acessar sua conta.</p>
+        {error && <Alert variant="danger">{error}</Alert>}
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="formBasicUsername">
+            <Form.Label>Nome de Usuário</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Digite seu usuário"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </Form.Group>
 
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Senha</Form.Label>
-        <Form.Control
-          type="password"
-          placeholder="Digite sua senha"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Senha</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Digite sua senha"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
 
-      <Button variant="primary" type="submit">
-        Entrar
-      </Button>
-    </Form>
-    </>
+          <Button variant="primary" type="submit">
+            Entrar
+          </Button>
+        </Form>
+      </div>
+    </div>
   );
 }
 
